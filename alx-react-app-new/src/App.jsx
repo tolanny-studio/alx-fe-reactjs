c0917633866f3de52b5668544a2390c4937bcf3e
tolanny-studio <hellotolannystudio@gmail.com>
2026-01-16 12:43:04 +0100

    Delete Component

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState,useRef } from 'react'
+import { useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,7 +7,6 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
-import Counter from './components/Counter'
 
 
 function App() {
