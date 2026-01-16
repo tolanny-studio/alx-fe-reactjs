1734bc70ace286ef582d916d1000e1cb2fa9c1b0
tolanny-studio <hellotolannystudio@gmail.com>
2026-01-16 12:37:31 +0100

    Create and import counter components to App

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,7 +7,7 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
-import Counter from './components/counter'
+import Counter from './components/Counter'
 
 
 function App() {
