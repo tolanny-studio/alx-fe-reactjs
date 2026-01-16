cab92945af7b62b0f01a55ab1bf8046c117f8947
tolanny-studio <hellotolannystudio@gmail.com>
2026-01-16 12:32:54 +0100

    Create counter component

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,8 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
+import Counter from './components/counter'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,6 +19,7 @@ function App() {
       <WelcomeMessage />
 
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <Counter />
       
       <div>
         <a href="https://vite.dev" target="_blank">
