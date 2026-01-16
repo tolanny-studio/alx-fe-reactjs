505d043462816a2576f515aa5d85f3cd3bbd9e7a
tolanny-studio <hellotolannystudio@gmail.com>
2026-01-16 12:46:14 +0100

    Add Counter Comp

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,8 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
+import Counter from './components/counter'
+
 
 
 function App() {
