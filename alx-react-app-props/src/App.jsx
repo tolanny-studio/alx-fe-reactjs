3f63f43b6e021e78ad4229abfb4aa96cacaec625
tolanny-studio <hellotolannystudio@gmail.com>
2026-01-17 15:58:41 +0100

    implement context on the App component

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -8,9 +8,9 @@ function App() {
 
   return (
     <>
-      <UserContext>
+      <UserContext.provider value={userData}>
         <ProfilePage />
-      </UserContext>
+      </UserContext.provider>
       
     </>
   );
