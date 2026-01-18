b84503217883eda422018aa88cd98d4aecf9e9af
tolanny-studio <hellotolannystudio@gmail.com>
2026-01-18 13:51:34 +0100

    Add pages to component folder

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Home";
-import About from "./About";
-import Services from "./Services";
-import Contact from "./Contact";
+import Home from "./components/Home";
+import About from "./components/About";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
 function App() {
   return (
