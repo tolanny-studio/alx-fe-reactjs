fe9d3f73f918144ea00a6dcd633ab82226cc645d
tolanny-studio <hellotolannystudio@gmail.com>
2026-01-24 18:18:18 +0100

    fix bug for initial setup

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,21 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
-import About from "./components/About";
-import Services from "./components/Services";
-import Contact from "./components/Contact";
-import Navbar from "./components/Navbar";
+import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeList from "./components/RecipeList";
 function App() {
   return (
     <>
-      <BrowserRouter>
-      <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </BrowserRouter>
+      <AddRecipeForm />
+      <RecipeList />
     </>
   );
 }
