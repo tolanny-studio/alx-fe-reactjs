8b1142b4eb738eb2b2b87bb38360bc344f4bbb81
tolanny-studio <hellotolannystudio@gmail.com>
2026-01-24 18:22:42 +0100

    Implement  recipe store in App component

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeList from "./components/RecipeList";
+import { useRecipeStore } from "./components/recipeStore";
 function App() {
   return (
     <>
