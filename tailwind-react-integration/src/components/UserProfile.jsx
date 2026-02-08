6504c3421bc3a9cda1184919120ccdca7614f336
tolanny-studio <hellotolannystudio@gmail.com>
2026-02-09 00:10:23 +0100

    Enhance responsiveness of UserProfile component

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -3,14 +3,11 @@ function UserProfile() {
     <div
       className="
         bg-gray-100
-        p-4 md:p-8
+        sm:p-4 md:p-8
         max-w-xs md:max-w-sm
         mx-auto my-20
         rounded-lg
         shadow-lg
-        hover:shadow-xl
-        transition-shadow
-        duration-300
       "
     >
       <img
@@ -18,24 +15,18 @@ function UserProfile() {
         alt="User"
         className="
           rounded-full
-          w-24 h-24 md:w-36 md:h-36
+          sm:w-24 sm:h-24
+          md:w-36 md:h-36
           mx-auto
-          transition-transform
-          duration-300
-          ease-in-out
-          hover:scale-110
         "
       />
 
       <h1
         className="
-          text-lg md:text-xl
+          sm:text-lg md:text-xl
           text-blue-800
           my-4
           text-center
-          hover:text-blue-500
-          transition-colors
-          duration-300
         "
       >
         John Doe
@@ -43,7 +34,7 @@ function UserProfile() {
 
       <p
         className="
-          text-sm md:text-base
+          sm:text-sm md:text-base
           text-gray-600
           text-center
         "
