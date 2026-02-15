b548e67379b1cea32b00ca9af09c12d6d8047989
tolanny-studio <hellotolannystudio@gmail.com>
2026-02-15 21:29:15 +0100

    Set up initial files and dependency

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import UserProfile from "./components/UserProfile";
+function App() {
+  return (
+    <>
+    <h1 className="text-blue-500">Tailwind Working</h1>
+    </>
+  );
+}
+
+export default App;
